401be723ff7a3ecf5f227b693d2932d11fbc7ef7
Matthew Blott <m4ttb@pm.me>
2025-12-23 16:38:05 +0000

    saved

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,8 +1,7 @@
 require 'zip'
 
 class SettingsController < ApplicationController
-  # skip_forgery_protection only: :download_images
-  skip_before_action :authenticate, only: [:download_images]
+  # skip_before_action :authenticate, only: [:download_images]
 
   def destroy
     @user = User.find(Current.user.id)
@@ -26,23 +25,6 @@ class SettingsController < ApplicationController
       filename: 'images.zip', 
       type: 'application/zip'
 
-    # redirect_to settings_path
-
-  end
-
-  def download_images_new
-    name = 'myfile'
-    extension = 'zip'
-    filename = "#{name}.#{extension}"
-
-    folder_path = Rails.root.join('uploads', Current.user.id.to_s)
-    path = Rails.root.join(folder_path, filename)
-    send_file path,
-      filename: filename,
-      # type: 'text/plain',
-      type: 'application/zip'
-      # type: 'image/png'
-      # , disposition: 'attachment'
   end
 
 end
