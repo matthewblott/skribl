e1866ce12459930451946dc30f2ea0f86325b704
Matthew Blott <m4ttb@pm.me>
2026-09-07 11:55:57 +0100

    If the user is signed in either by device or user account they are redirected to the user\'s home page when they navigate to the root.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,26 +7,47 @@ class ApplicationController < ActionController::Base
   private
 
   def load_current_user
-    if (session_record = Session.find_by_id(cookies.signed[:session_token]))
+    session_record = Session.find_by_id(cookies.signed[:session_token])
+
+    unless session_record.nil? 
       Current.session = session_record
       Current.user = session_record.user
-    elsif (token = cookies.encrypted[:device_token]).present? &&
-        (user = User.find_by(device_token: token))
-      Current.user = user
+      return
     end
+
+    token = cookies.encrypted[:device_token]
+
+    unless token.present?
+      return
+    end
+
+    user = User.find_by(device_token: token)
+
+    unless user
+      return
+    end
+
+    Current.user = user
   end
 
   def authenticate_user!
-    return if Current.user
+    if Current.user
+      return
+    end
+
     redirect_to info_path
   end
 
   def authorize_user!
-    return unless params[:user_id].present?
-    
-    unless Current.user.id == params[:user_id].to_i
-      redirect_to home_path, alert: "Not authorised."
+    unless params[:user_id].present?
+      return
     end
+    
+    if Current.user.id == params[:user_id].to_i
+      return
+    end
+
+    redirect_to home_path, alert: "Not authorised."
   end
 
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,16 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
   skip_before_action :authorize_user!
+
+  def info
+    user = Current.user
+    
+    if user.nil?
+      return
+    end
+
+    redirect_to user_home_path(user.id)
+
+  end
+
 end
